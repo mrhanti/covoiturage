69081106d89f5c2b1ba526b0672b539c548474d8
mrhanti <mr.el.hanti@gmail.com>
2012-12-27 18:29:45 +0100

    deleting user id generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,11 @@ class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email
   attr_accessible :phone_number, :year_of_birth, :gender
 
-  has_many :ads, :dependent => :destroy
-
-  before_create :generate_encrypted_id
+  has_many :ads, :dependent => :destroy 
 
   validates :email, :presence => true
   
   def full_name
     "#{self.first_name} #{self.last_name}"
-  end
-
-  def generate_encrypted_id
-  	self.id = Digest::SHA1.hexdigest(self.email)
   end
 end
