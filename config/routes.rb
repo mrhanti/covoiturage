271197e9d96674529dad8cc7c766d594f89234b6
mrhanti <mr.el.hanti@gmail.com>
2013-01-03 00:38:24 +0100

    add user/edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Covoiturage::Application.routes.draw do
   
-  resources :users, :only => [:new, :create, :edit, :update] do
+  resources :users, :only => [:new, :create, :update, :edit] do
     resources :ads, :only => [:index, :new, :create]
   end
 
   post "/search" => "ads#search"
+  # get  "/profile" => "users#edit", :as => "user_profile"
   
   # /profile
   # /search
