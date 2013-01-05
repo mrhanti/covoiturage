b92b378d0e75bffac041861572bfe18022576c46
mrhanti <mr.el.hanti@gmail.com>
2013-01-05 04:03:17 +0100

    add token confirmation generation when creating new ads

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,15 +3,22 @@ class Ad < ActiveRecord::Base
   attr_accessible :seat_limit, :price_per_seat, :highway, :itinerary
 
   belongs_to :user
-  has_one :confirmation
+  has_one :confirmation, :dependent => :destroy
+
+  before_save :generate_confirmation_code
   
   validates :destination, :departure, :departure_on, :departure_on, :presence => true
+  validates :seat_limit, :price_per_seat, :presence => true
 
   def generate_confirmation_code
     self.confirmation = Confirmation.create
   end
 
-  def dd
+  def belongs_to?(user)
+    self.user == user
+  end
+  
+  def to_s
   	"#{departure} -> #{destination} on #{departure_on} at #{departure_at}"
   end
 end
