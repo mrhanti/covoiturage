d2275efe933877cacc64f489954f14d1de970070
mrhanti <mr.el.hanti@gmail.com>
2013-01-15 00:07:24 +0100

    clean up

--- db/migrate/20130107222232_add_owner_to_ads.rb
+++
@@ -1,9 +0,0 @@
-class AddOwnerToAds < ActiveRecord::Migration
-  def change
-    add_column :ads, :owner_id, :boolean, :default => false
-  end
-
-  def down
-  	remove_column :ads, :owner_id, :boolean
-  end
-end
--- db/migrate/20130107225620_change_ads_users_name.rb
+++
@@ -1,8 +0,0 @@
-class ChangeAdsUsersName < ActiveRecord::Migration
-  def up
-  	rename_table :users_ads, :ads_users
-  end
-
-  def down
-  end
-end
